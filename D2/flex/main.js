8b038d231b9f48cc12bd877c3e37035a63485155
Alessandro Orlandelli <aleorla2909@gmail.com>
2023-06-25 11:52:05 +0200

    fine esercisio flex

---
+++ D2/flex/main.js
@@ -0,0 +1,11 @@
+function openInfo() {
+    document.getElementById("article").style.display = 'flex';
+}
+
+function closedInfo() {
+    document.getElementById("article").style.display = 'none';
+}
+
+function cart(){
+    window.alert('NON ESISTE IL CARRELLO!!!');
+}
