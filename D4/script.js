30afbd49a2557b0214fa2cee32c9acc071d1f05c
Alessandro Orlandelli <aleorla2909@gmail.com>
2023-07-01 19:34:50 +0200

    modifiche D4

---
+++ D4/script.js
@@ -0,0 +1,8 @@
+function order() {
+   if(document.querySelector('#order').style.display === "block"){
+      document.querySelector('#order').style.display = "none";
+   } else {
+      document.querySelector('#order').style.display = "block";
+   }
+      
+}
